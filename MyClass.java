da8ab7f0f32c8914504166ef11ef79c4cd7a0d03
tonycituk <tonycituk@gmail.com>
2022-02-15 11:46:21 -0600

    Created the example java class to document with Javadoc

---
+++ MyClass.java
@@ -0,0 +1,49 @@
+/**
+ * MyClass is an example class...
+ * 
+ * @author tonycituk
+ * 
+ * 
+ */
+public class MyClass {
+
+    /**
+     * A field of the class
+     */
+    private String field;
+
+    /**
+     * <p>
+     * This is a simple description of the method. . .
+     * <a href="https://media1.giphy.com/media/tHVfwcmpBf509zs7V1/giphy.gif">Check
+     * this out!</a>
+     * </p>
+     * 
+     * @return the data stored on the field String.
+     * @see <a href=
+     *      "https://media1.giphy.com/media/tHVfwcmpBf509zs7V1/giphy.gif">Check this
+     *      out too!</a>
+     * @since 1.0
+     */
+    public String getField() {
+        return this.field;
+    }
+
+    /**
+     * <p>
+     * This is another simple description of another method. . .
+     * <a href="https://media1.giphy.com/media/tHVfwcmpBf509zs7V1/giphy.gif">Check
+     * this out!</a>
+     * </p>
+     * 
+     * @param field the string to store on field.
+     * @see <a href=
+     *      "https://media1.giphy.com/media/tHVfwcmpBf509zs7V1/giphy.gif">Check this
+     *      out too!</a>
+     * @since 1.0
+     */
+    public void setField(String field) {
+        this.field = field;
+    }
+
+}
